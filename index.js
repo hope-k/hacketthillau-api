072899a54696663204b1000221a3a3a921e25160
hope-k <hopekumordzie@gmail.com>
2023-01-15 00:44:11 +0000

    new cors url

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 //... other middlewares
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://hacketthillau.com', 'https://nachtnebeltrust.com', 'https://familytrustbank.netlify.app', 'https://hacketthillau-api.vercel.app'  ],
+    origin: ['http://localhost:3000', 'https://hacketthillau.com', 'https://nachtnebeltrust.com', 'https://www.nachtnebeltrust.com', 'https://familytrustbank.netlify.app', 'https://hacketthillau-api.vercel.app'  ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, // enable set cookie
 }));
