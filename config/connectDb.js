5b3bfed9b95a1aef3db9f373bfafb3f615817e82
hope-k <hopekumordzie@gmail.com>
2023-01-15 16:18:32 +0000

    fix mongo uri

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose')
 const colors = require('colors')
 const connection = mongoose.connection
 
+const production = process.env.NODE_ENV === 'production'
 
-mongoose.connect(process.env.LOCAL_MONGO_URI)
+mongoose.connect( production ? process.env.MONGO_URI : process.env.LOCAL_MONGO_URI, )
     .then(() => console.log('MongoDB Connected'.bgMagenta.white.bold))
     .catch(err => console.log(`${err.message}`.bgRed.white.bold))
 
