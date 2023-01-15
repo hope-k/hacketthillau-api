13ad7fb991cbaf60e395869a3a73bb4b4e73aed3
hope-k <hopekumordzie@gmail.com>
2023-01-15 03:00:40 +0000

    cors check

--- index.js
+++ index.js
@@ -19,11 +19,17 @@ app.set('trust proxy', 1)
 app.use(cookieParser())
 //... other middlewares
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // enable set cookie
-}));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+
+
+
 
 
 
