fb3bfcfb647f284f45d491f1dba2c566cb697fbe
hope-k <hopekumordzie@gmail.com>
2023-01-15 03:14:00 +0000

    cors checks..

--- index.js
+++ index.js
@@ -19,19 +19,10 @@ app.set('trust proxy', 1)
 app.use(cookieParser())
 //... other middlewares
 
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://www.nachtnebeltrust.com");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-
-
-
-
+app.use(cors({
+    origin: 'https://www.nachtnebeltrust.com',
+    credentials: true, // enable set cookie
+}));
 
 
 
